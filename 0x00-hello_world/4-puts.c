9f307b6e383e87a34d32380270602b83e241c664
Udegbue <princewillmatrix47@gmail.com>
2022-06-30 15:42:54 -0700

    corr

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <studio>
+/*
+*main - Entry point
+*Return: Always 0 (sucess)
+*/
+int main(void)
+{puts("\n "Programming is like  building a multilingual puzzle");
+	return (0)
+}
