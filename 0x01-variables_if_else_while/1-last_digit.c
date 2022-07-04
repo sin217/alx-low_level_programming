c5a3d2c70a9c791f20807017f73aec2a8b2f4bea
Udegbue <princewillmatrix47@gmail.com>
2022-07-04 16:15:16 -0700

    vov

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 /**
  * main - Entry point
@@ -9,8 +9,7 @@
 int main(void)
 {
 int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n % 10 > 5)
