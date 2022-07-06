9b6846674e0dffe337bae5c7b54f79c57430b07b
Udegbue <princewillmatrix47@gmail.com>
2022-07-06 14:51:38 -0700

    noon

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,9 @@
 #include "main.h"
 /**
- *
- * * main - prints putchar
- *
- * * Return: always 0
- *
- * */
-
+ * main - prints putchar
+ * Return: always 0
+ */
 int main(void)
-
 {
 _putchar('_');
 _putchar('p');
