122bd6fb8e5710512202a250bf9886215326f7d5
Udegbue <princewillmatrix47@gmail.com>
2022-07-06 17:24:41 -0700

    moko

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,13 +15,13 @@ _putchar(k + '0');
 if (y != 0 && k < 10)
 {
 _putchar(',');
-_putchar(' ')
+_putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
 }
 else if (k >= 10)
 {
-_putchar(',')
+_putchar(',');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 1) + '0');
