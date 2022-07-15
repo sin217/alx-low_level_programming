a5e10c7c7df80f7c676fce2f8e8ca667760b8663
Udegbue <princewillmatrix47@gmail.com>
2022-07-15 08:29:14 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,7 +8,7 @@
  *
  * Return: returns pointer to @dest
  */
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
 	int index = 0, dest_len = 0;
 
@@ -17,6 +17,6 @@ char *_strcat(char *dest, char *src);
 
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
-	
+
 	return (dest);
 }
